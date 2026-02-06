9ea119f114b00ece59bb761ac4c3df3afa4a4752
Max Harari <meltedd@protonmail.com>
2026-02-06 15:06:25 -0500

    refactor: remove redundant isMounted guard in MusicPlayer
    dynamic(..., { ssr: false }) already prevents server rendering,
    making the isMounted state and useEffect unnecessary.

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 import { RHAP_UI } from 'react-h5-audio-player'
 import './MusicPlayer.css'
@@ -16,32 +15,24 @@ const AudioPlayer = dynamic(() => import('react-h5-audio-player'), {
 })
 
 export function MusicPlayer({ src, title }: MusicPlayerProps) {
-  const [isMounted, setIsMounted] = useState(false)
-
-  useEffect(() => {
-    setIsMounted(true)
-  }, [])
-
   return (
     <div className="not-prose my-[clamp(1.25rem,1.75vw,1.75rem)]">
       <div className="music-player-title">
         {title}
       </div>
       <div className="music-player-container">
-        {isMounted && (
-          <AudioPlayer
-            src={src}
-            showJumpControls={false}
-            customAdditionalControls={[]}
-            customVolumeControls={[]}
-            customProgressBarSection={[
-              RHAP_UI.CURRENT_TIME,
-              RHAP_UI.PROGRESS_BAR,
-            ]}
-            layout="horizontal-reverse"
-            autoPlayAfterSrcChange={false}
-          />
-        )}
+        <AudioPlayer
+          src={src}
+          showJumpControls={false}
+          customAdditionalControls={[]}
+          customVolumeControls={[]}
+          customProgressBarSection={[
+            RHAP_UI.CURRENT_TIME,
+            RHAP_UI.PROGRESS_BAR,
+          ]}
+          layout="horizontal-reverse"
+          autoPlayAfterSrcChange={false}
+        />
       </div>
     </div>
   )
